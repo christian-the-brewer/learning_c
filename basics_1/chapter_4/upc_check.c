bf9a844c23650eae2b065c76440cc9affb287936
christian-the-brewer <brewer.christiang@gmail.com>
2023-03-04 08:54:52 -0500

    chapter 5 programming exercises

--- basics_1/chapter_4/upc_check.c
+++ basics_1/chapter_4/upc_check.c
@@ -7,17 +7,20 @@
 
 int main(void)
 {
-    int first, i1, i2, i3, i4, i5, group1, j1, j2, j3, j4, j5, group2, total, last;
+    int first, i1, i2, i3, i4, i5, group1, j1, j2, j3, j4, j5, group2, total, last, check;
 
-    printf("Enter 11 digit UPC: ");
-    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &first, &i1, &i2, &i3, &i4, &i5, &j1, &j2, &j3, &j4, &j5);
+    printf("Enter 12 digit UPC: ");
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%d", &first, &i1, &i2, &i3, &i4, &i5, &j1, &j2, &j3, &j4, &j5, &check);
 
     group1 = first + i2 + i4 + j1 + j3 + j5;
     group2 = i1 + i3 + i5 + j2 + j4;
     total = 3 * group1 + group2;
     last = (9 - ((total - 1) % 10));
 
-    printf("Check digit: %d\n", last);
+    if (check == last)
+        printf("UPC Valid");
+    else
+        printf("UPC Invalid");
 
     return 0;
 }
