7df1da0a3cb1f9f2e5cd28bda2a954f83e595b07
christian-the-brewer <brewer.christiang@gmail.com>
2023-03-12 15:16:38 -0400

    updates broker to have do loop

--- basics_1/chapter_5/broker_fee.c
+++ basics_1/chapter_5/broker_fee.c
@@ -7,38 +7,42 @@
 
 int main(void)
 {
-    float commission, value, shares, price, rival_commission;
+    float commission = 0, value, shares, price, rival_commission;
 
-    printf("Enter number of shares: ");
-    scanf("%f", &shares);
-    printf("Enter price of share: ");
-    scanf("%f", &price);
+    do
+    {
 
-    value = shares * price;
+        printf("Enter number of shares: ");
+        scanf("%f", &shares);
+        printf("Enter price of share: ");
+        scanf("%f", &price);
 
-    if (value < 2500.00f)
-        commission = 30.00f + .017f * value;
-    else if (value < 6250.00f)
-        commission = 56.00f + .0066f * value;
-    else if (value < 20000.00f)
-        commission = 76.00f + .0034f * value;
-    else if (value < 6250.00f)
-        commission = 56.00f + .0066f * value;
-    else if (value < 50000.00f)
-        commission = 100.00f + .0022f * value;
-    else
-        commission = 255.00f + .0009f * value;
+        value = shares * price;
 
-    if (commission < 39.00f)
-        commission = 39.00f;
+        if (value < 2500.00f)
+            commission = 30.00f + .017f * value;
+        else if (value < 6250.00f)
+            commission = 56.00f + .0066f * value;
+        else if (value < 20000.00f)
+            commission = 76.00f + .0034f * value;
+        else if (value < 6250.00f)
+            commission = 56.00f + .0066f * value;
+        else if (value < 50000.00f)
+            commission = 100.00f + .0022f * value;
+        else
+            commission = 255.00f + .0009f * value;
 
-    printf("Commission: $%.2f\n", commission);
+        if (commission < 39.00f)
+            commission = 39.00f;
 
-    if (shares < 2000)
-        rival_commission = 33.00f + .03f * shares;
-    else
-        rival_commission = 33.00f + .02f * shares;
-    printf("Rival Commission: $%.2f\n", rival_commission);
+        printf("Commission: $%.2f\n", commission);
+
+        if (shares < 2000)
+            rival_commission = 33.00f + .03f * shares;
+        else
+            rival_commission = 33.00f + .02f * shares;
+        printf("Rival Commission: $%.2f\n", rival_commission);
+    } while (shares != 0);
 
     return 0;
 }
