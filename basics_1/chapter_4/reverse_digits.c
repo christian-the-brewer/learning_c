57c40132d5f5f6c5b44caf06e2478af2934f6840
christian-the-brewer <brewer.christiang@gmail.com>
2023-03-13 12:09:23 -0400

    updates chapter 4 reverse with a loop

--- basics_1/chapter_4/reverse_digits.c
+++ basics_1/chapter_4/reverse_digits.c
@@ -7,11 +7,25 @@
 
 int main(void)
 {
-    int i, j, k;
+    // int i, j, k;
 
-    printf("Enter a three digit number: ");
-    scanf("%1d%1d%1d", &i, &j, &k);
+    // printf("Enter a three digit number: ");
+    // scanf("%1d%1d%1d", &i, &j, &k);
 
-    printf("%d%d%d\n", k, j, i);
+    // printf("%d%d%d\n", k, j, i);
+
+    int num, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("%d reversed: ", num);
+
+    do
+    {
+        i = num % 10;
+        num /= 10;
+        printf("%d", i);
+    } while (num > 0);
+
     return 0;
 }
